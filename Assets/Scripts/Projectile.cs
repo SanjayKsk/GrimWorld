c56c0b3d4a8a96e355158e9f87eda5f52a755eef
SanjayKsk <skhaira98@gmail.com>
2024-03-09 19:18:58 +0800

    created melee enemy sprite

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,14 +6,21 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
     public float lifeTime;
-
+    public GameObject burst;
     private void Start()
     {
-        Destroy(gameObject, lifeTime);
+        Invoke("DestroyProjectile", lifeTime);
     }
 
     private void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+
+    void DestroyProjectile()
+    {
+        Instantiate(burst, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+
     }
 }
