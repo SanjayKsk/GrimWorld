213d8893c5cf41d7e750223f8080808ae36e634d
SanjayKsk <skhaira98@gmail.com>
2024-03-10 16:27:08 +0800

    melee enemy mechanics

--- Assets/Scripts/EnemyControl.cs
+++ Assets/Scripts/EnemyControl.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class EnemyControl : MonoBehaviour
 {
     public int health;
+    [HideInInspector]
+    public Transform player;
+    public float speed; 
+    public float timeBetweenAttacks;
 
+    public int damage;
+    
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     public void TakeDamage(int damage)
     {
         health -= damage;
